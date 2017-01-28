9f2d989e9e50b2618ed1401aafe8aa8b4de4b053
velopert <public.velopert@gmail.com>
2017-01-28 12:31:25 +0900

    react-router & hot-loader 오류 해결

--- develoxy-frontend/src/index.js
+++ develoxy-frontend/src/index.js
@@ -24,6 +24,25 @@ ReactDOM.render(
 
 
 if(module.hot) {
+
+    /**
+     * Warning from React Router, caused by react-hot-loader.
+     * The warning can be safely ignored, so filter it from the console.
+     * Otherwise you'll see it every time something changes.
+     * See https://github.com/gaearon/react-hot-loader/issues/298
+     */
+    
+    const orgError = console.error;
+    console.error = (...args) => {
+        if (args && args[0] && typeof args[0] === 'string' &&
+            args[0].indexOf('You cannot change <Router routes>;') > -1) {
+            // React route changed
+        } else {
+            // Log the error as normal
+            orgError.apply(console, args);
+        }
+    };
+
     module.hot.accept('./containers/Root', () => {
         const NextRoot = require('./containers/Root').default;
         ReactDOM.render(
@@ -33,62 +52,3 @@ if(module.hot) {
         )
     })
 }
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { AppContainer } from 'react-hot-loader'
-// import { Provider } from 'react-redux';
-
-// import firebase from 'firebase';
-// import firebaseConfig from '../config/firebase';
-
-
-// import Root from 'containers/Root';
-// import Routes from 'containers/Routes';
-// import App from 'containers/App';
-
-// // redux
-// import configureStore from 'redux/configureStore';
-
-// firebase.initializeApp(firebaseConfig);
-
-// const store = configureStore();
-// const rootElement = document.getElementById('root');
-
-
-// ReactDOM.render(
-//     <AppContainer>
-//         <App/>
-//     </AppContainer>, rootElement
-// )
-
-// if(module.hot) {
-//     module.hot.accept('./containers/App', () => {
-//         const NextApp = require('./containers/App').default;
-//         ReactDOM.render(
-//             <AppContainer>
-//                 <NextApp/>
-//             </AppContainer>, rootElement
-//         )
-//     })
-// }
-
-// // ReactDOM.render(
-// //     (
-// //     <AppContainer>
-// //         <Root store={store}/>
-// //     </AppContainer>
-// //     ), rootElement
-// // );
-
-// // if(module.hot) {
-// //     module.hot.accept('./containers/Root', () => {
-// //         const NextRoot = require('./containers/Root').default;
-// //         ReactDOM.render(
-// //             <AppContainer>
-// //                 <NextRoot store={store}/>
-// //             </AppContainer>, rootElement
-// //         );
-// //     })
-// // }
--- develoxy-frontend/src/routes.js
+++ develoxy-frontend/src/routes.js
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import { Router, Route, IndexRoute } from 'react-router';
-
-// import App from './App';
-// import { MainRoute, AuthRoute } from 'containers/routes';
-
-
-// const Routes = ({history}) => (
-//     <Router history={history}>
-//         <Route path="/" component={App}>
-//             <IndexRoute component={MainRoute}/>
-//             <Route path="auth" component={AuthRoute}/>}
-//         </Route>
-//     </Router>
-// );
-
-// export default Routes;
-
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
