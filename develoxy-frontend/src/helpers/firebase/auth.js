d17196da6e9e52395e578fa55e37fab9e6dd75ff
velopert <public.velopert@gmail.com>
2017-01-29 04:43:14 +0900

    계정 링크 재구현 (이제 제대로 됨)

--- develoxy-frontend/src/helpers/firebase/auth.js
+++ develoxy-frontend/src/helpers/firebase/auth.js
@@ -13,23 +13,21 @@ const auth = (function() {
             return firebase.auth().signInWithPopup(providers.github);
         },
         google: () => {
-            const provider = new firebase.auth.GoogleAuthProvider();
             return firebase.auth().signInWithPopup(providers.google);
         },
         facebook: () => {
-            const provider = new firebase.auth.FacebookAuthProvider();
             return firebase.auth().signInWithPopup(providers.facebook);
         },
         logout: () => {
             return firebase.auth().signOut()
         },
         resolveDuplicate: async (error) => {
-            const { pendingCred, email } = error;
+            const { credential, email } = error;
             const existingProviders = await firebase.auth().fetchProvidersForEmail(email);
             // 비밀번호를 사용하여 인증하는 경우는 없으니, 소셜 계정을 사용하는거로 간주 (당연히)        
             const provider = existingProviders[0].split('.')[0];
             const result = await firebase.auth().signInWithPopup(providers[provider]);
-            return result.user.link(pendingCred);
+            return result.user.link(credential);
         },
         authStateChanged: (callback) => {
             // 콜백은 firebaseUser 를 파라미터로 받음
