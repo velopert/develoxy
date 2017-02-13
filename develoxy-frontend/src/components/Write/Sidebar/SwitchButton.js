929058c3ca52859204595ea535deb7cf2c66425c
velopert <public.velopert@gmail.com>
2017-02-14 00:00:35 +0900

    스위치 버튼

--- develoxy-frontend/src/components/Write/Sidebar/SwitchButton.js
+++ develoxy-frontend/src/components/Write/Sidebar/SwitchButton.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { Icon } from 'semantic-ui-react';
+
 
 const SwitchButton = () => {
     return (
         <div className="switch-button">
-            <Icon name="exchange"/>
-            <div><b>록시패드</b>로 전환</div>
+            <div className="text"><b>록시패드</b>로 전환</div>
         </div>
     );
 };
