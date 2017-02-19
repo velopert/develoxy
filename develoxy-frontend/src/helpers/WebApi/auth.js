3ce400d04160eff37a694b08aa9011c3888b7f73
velopert <public.velopert@gmail.com>
2017-02-20 01:19:36 +0900

    회원인증 관련 API 작성

---
+++ develoxy-frontend/src/helpers/WebApi/auth.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const checkUsername = (username) => {
+    return axios.get(`/api/auth/check-username/${username}`);
+}
+
+export const register = (username) => {
+    return axios.post('/api/auth/register', {
+        username
+    });
+}
