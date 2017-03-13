16c026d6c552ea80432b4c4ed5812fa13d3f405a
velopert <public.velopert@gmail.com>
2017-03-13 23:24:22 +0900

    Redis Wrapper 작성

---
+++ develoxy-backend/app/helpers/cache.js
@@ -0,0 +1,35 @@
+// redis wrapper
+const redis = require('redis');
+
+module.exports = (() => {
+
+    let client = null;
+
+    return {
+        connect: () => {
+            client = redis.createClient();
+
+            client.on('connect', function() {
+                console.log('Connected to Redis Server');
+            });
+        },
+        set: (key, value, stringify = true) => {
+            client.set(key, stringify ? JSON.stringify(value) : value);
+        },
+        get: (key, parseJSON = true) => {
+            const promise = (
+                (resolve, reject) => {
+                    client.get(key, (err, reply) => {
+                        if(err) { reject(err); }
+                        resolve(parseJSON ? JSON.parse(reply) : reply);
+                    });
+                }
+            );
+
+            return promise;
+        },
+        test: () => {
+            console.log(client);
+        }
+    }
+})();
