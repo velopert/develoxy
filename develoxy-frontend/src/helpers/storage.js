f4819fa9ea3dffcd7f1b2996179a3ccdfed250ef
velopert <public.velopert@gmail.com>
2017-03-26 00:44:39 +0900

    스토리지 기능 개선

--- develoxy-frontend/src/helpers/storage.js
+++ develoxy-frontend/src/helpers/storage.js
@@ -1,14 +1,22 @@
+// 로컬 스토리지에 JSON 형태로 저장 / 불러오기 / 삭제 헬퍼
+
 const storage = {};
 
 storage.set = (key, object) => {
-    localStorage[key] = JSON.stringify(object);
+    localStorage[key] = (typeof object) === 'string' ? object : JSON.stringify(object);
 }
 
 storage.get = (key) => {
     if(!localStorage[key]) {
-        return undefined;
+        return null;
     }
-    return JSON.parse(localStorage[key]);
+
+    try {
+        const parsed = JSON.parse(localStorage[key]);
+        return parsed;
+    } catch(e) {
+        return localStorage[key];
+    }
 }
 
 storage.remove = (key) => {
